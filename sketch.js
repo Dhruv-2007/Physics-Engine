f8492b0ba46f741924cad00cf78137645f53a223
Dhruv-2007 <71203997+Dhruv-2007@users.noreply.github.com>
2020-10-17 10:35:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,22 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine,world;
+
+var ground;
+
 function setup() {
   createCanvas(400,400);
+  
+  engine = Engine.create();
+  world = engine.world;
+  
+  ground =  Bodies.rectangle(0,400,400,10);
+  
+  World.add(world,ground);
+  
+  console.log(ground);
 }
 
 function draw() {
